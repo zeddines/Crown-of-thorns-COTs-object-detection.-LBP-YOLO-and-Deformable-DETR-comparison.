479e77b9e7ffd15f6e782f8ce8c231b9e6f1d5ad
zeddines <zeddines1999@gmail.com>
2022-10-21 17:59:47 +1100

    yolo pre processing done

--- YOLO_preprocess.py
+++ YOLO_preprocess.py
@@ -11,6 +11,8 @@ training_ann_file_path = "training-annotation-YOLO"
 test_ann_fille_path = "test-annotation-YOLO"
 training_img_file_path = "training-images-YOLO"
 test_img_file_path = "test-images-YOLO"
+train_split = 0.8
+#train_w_wo_class_split = 0.5
 
 img_height = 720
 img_width = 1280
@@ -31,7 +33,7 @@ def split_annotate_with_without_class(df):
 
 def split_annotate_test_train(annotate):
     s = pd.Series(annotate)
-    training_annotate, test_annotate = [i.to_dict() for i in train_test_split(s, train_size = 0.8)]
+    training_annotate, test_annotate = [i.to_dict() for i in train_test_split(s, train_size = train_split)]
     return training_annotate, test_annotate
 
 def create_annotation_folder(ann_data, foldername):
@@ -58,18 +60,10 @@ def find_object_params(x, y, width, height):
     return  (x+width/2)/img_width, (y+height/2)/img_height,width/img_width, height/img_height
 
 def create_data_folders(train_ann, test_ann):
-    create_annotation_folder(train_ann, "train-annotations")
-    create_annotation_folder(test_ann, "test-annotations")
-    create_img_folder(train_ann, "train-images")
-    create_img_folder(test_ann, "test-images")
-    
-
-"""def remove_img_without_classes(df):
-    out_dict = {}
-    for i in range(len(df.index)):
-        if df['annotations'][i]:
-            out_dict[df['image_id'][i]] = df['annotations'][i]
-    return out_dict"""
+    create_annotation_folder(train_ann, training_ann_file_path)
+    create_annotation_folder(test_ann, test_ann_fille_path)
+    create_img_folder(train_ann, training_img_file_path)
+    create_img_folder(test_ann, test_img_file_path)
 
 df = pd.read_csv(os.path.join(data_path, 'train.csv'), converters={'annotations': lambda x: convert_annotaions_to_dicts(x)})
 
