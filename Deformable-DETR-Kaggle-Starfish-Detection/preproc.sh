a003b6e1c407a18808e22d9c7bdb8e4f202d78b3
Christopher Lee <christopher5a219@hotmail.com>
2022-11-06 03:13:29 +1100

    Updated preproc.sh

--- Deformable-DETR-Kaggle-Starfish-Detection/preproc.sh
+++ Deformable-DETR-Kaggle-Starfish-Detection/preproc.sh
@@ -2,6 +2,8 @@
 
 patch --forward -d Deformable-DETR -p1 < files/patch0.patch
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1J0Wa6uc-X7EShMH6qOsckRaB8KvXZbn-' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1J0Wa6uc-X7EShMH6qOsckRaB8KvXZbn-" -O files/models.tar.xz && rm -rf /tmp/cookies.txt
+#Use this instead if want full checkpoints
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bUa-xZhW6YfBnj2vA9ZWXdXSZEFgw4oR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1bUa-xZhW6YfBnj2vA9ZWXdXSZEFgw4oR" -O files/models.tar.xz && rm -rf /tmp/cookies.txt
 tar -xvf files/models.tar.xz -C Deformable-DETR
 pip3 install --user -r pre_proc/requirements.txt
 var1=$( pip3 show albumentations | grep 'Location:' | cut -d ':' -d ' ' -f 2)
