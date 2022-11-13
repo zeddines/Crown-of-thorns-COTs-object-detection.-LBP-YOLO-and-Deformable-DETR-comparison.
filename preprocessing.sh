cf8c7ca5abdf6d4ead9ee168ba1b3788421d15ec
Christopher Lee <christopher5a219@hotmail.com>
2022-11-14 01:23:36 +1100

    Updated

--- preprocessing.sh
+++ preprocessing.sh
@@ -2,16 +2,16 @@
 
 mv tensorflow-great-barrier-reef Deformable-DETR-Kaggle-Starfish-Detection/files/
 cd Deformable-DETR-Kaggle-Starfish-Detection
-sh pre_proc/detr_preproc.sh
+sh preproc.sh
 cd ../YOLO
 if [ `uname` = "msys" ] ;
 then
-    dos2unix patch0.patch
+    unix2dos patch0.patch
 elif [ `uname` = "cygwin" ];
 then
-    dos2unix patch0.patch
-else
     unix2dos patch0.patch
+else
+    dos2unix patch0.patch
 fi
 sh process.sh
 cd ..
